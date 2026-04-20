3fd05658a1ba1ec27805d38546d8705c004ed674
lvegavaldez <vegaluis.v@gmail.com>
2026-04-20 01:42:42 -0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/alk/transactions/TransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.alk.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsApiApplication.class, args);
+	}
+
+}
